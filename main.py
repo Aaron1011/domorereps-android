7d3ffc24b14c1b3cd7b594540d2d3d88daaac972
Aaron Hill <aa1ronham@aol.com>
2013-05-11 19:25:54 -0400

    Added method for loading exercises into sqlite db

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+import yaml
+import sqlite3
+
 class HomeScreen(Screen):
     pass
 
@@ -18,6 +21,22 @@ class DomorerepsApp(App):
         sm.add_widget(ExercisesScreen(name='exercises'))
         self.load_exercises()
         return sm
+
+    def load_exercises(self):
+        with open('data/exercises.yml') as f:
+            data = yaml.load(f.read())
+            conn = sqlite3.connect('db.sqlite3')
+            c = conn.cursor()
+            c.execute('CREATE TABLE IF NOT EXISTS exercises (name text, \
+            weightless integer, unique (name))''')
+
+            for exercise in data.values():
+                name = exercise['name']
+                weightless = exercise['weightless']
+                c.execute('''INSERT OR IGNORE INTO exercises VALUES (?, ?)''',
+                        (name, weightless))
+            conn.commit()
+        conn.close()
 
 if __name__ == "__main__":
     DomorerepsApp().run()
