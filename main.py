87703e2bba33331eb5c508f1bf598f041dd2d40e
Aaron Hill <aa1ronham@aol.com>
2013-05-11 18:24:17 -0400

    Set up HomeScreen with ScreenManager

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ kivy.require('1.6.0')
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class HomeScreen(BoxLayout):
+class HomeScreen(Screen):
     pass
-
 
 class DomorerepsApp(App):
     def build(self):
-        return HomeScreen()
+        sm = ScreenManager()
+        sm.add_widget(HomeScreen(name='home'))
+        return sm
 
 if __name__ == "__main__":
     DomorerepsApp().run()
