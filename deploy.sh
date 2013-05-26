2e4778d26c317ee5953474ffeb4d206a44657495
Aaron Hill <aa1ronham@gmail.com>
2013-05-26 17:36:27 -0400

    Updated deploy script to build apk

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
-adb shell rm -r /sdcard/kivy/domorereps-android/.kivy
-adb push ~/repos/domorereps-android /sdcard/kivy/domorereps-android
+export ANDROIDSDK=~/Downloads/adt-bundle-linux-x86_64-20130219/sdk/
+export ANDROIDAPI=8
+export ANDROIDNDK=~/Downloads/android-ndk-r8e
+export ANDROIDNDKVER=8
+
+export PYTHON_DIST_PATH=~/Documents/kivy/python-for-android/dist/default/
+
+cd $PYTHON_DIST_PATH
+./build.py --dir ~/repos/domorereps-android/ --package org.domorereps.domorereps --name "Do More Reps" --version 1.0.0 --orientation portrait --permission INTERNET debug installd
