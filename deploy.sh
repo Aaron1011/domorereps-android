e3fb190a2cbe32fbc6b29ebc469140767f799923
Aaron Hill <aa1ronham@gmail.com>
2013-05-28 14:05:18 -0400

    Fixed invalid syntax in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,21 @@
-$ANDROIDSDK || export ANDROIDSDK=~/Downloads/adt-bundle-linux-x86_64-20130219/sdk/
-$ANDROIDAPI || export ANDROIDAPI=8
-$ANDROIDNDK || export ANDROIDNDK=~/Downloads/android-ndk-r8e
-$ANDROIDNDKVER || export ANDROIDNDKVER=8
+ANDROIDSDK=~/android-tools/adt-bundle-linux-x86_64-20130514/sdk
+ANDROIDNDK=~/android-tools/android-ndk-r8e
+PYTHON_DIST_PATH=~/python-for-android/dist/default
 
-$PYTHON_DIST_PATH || export PYTHON_DIST_PATH=~/Documents/kivy/python-for-android/dist/default/
+PATH=$ANDROIDNDK:$ANDROIDSDK/tools:$PATH
 
-$BUILD_TYPE || export BUILD_TYPE=debug
+BUILD_TYPE=release
 
-cd $PYTHON_DIST_PATH
+if [ -z "$ANDROIDSDK" ]; then export ANDROIDSDK=~/Downloads/adt-bundle-linux-x86_64-20130219/sdk/; fi
+if [ -z "$ANDROIDAPI" ]; then export ANDROIDAPI=8; fi
+if [ -z "$ANDROIDNDK" ]; then export ANDROIDNDK=~/Downloads/android-ndk-r8e; fi
+if [ -z "$ANDROIDNDKVER" ]; then export ANDROIDNDKVER=8; fi
 
-./build.py --dir ~/repos/domorereps-android/ --package org.domorereps.domorereps --name "Do More Reps" --version 1.0.0 --orientation portrait --permission INTERNET $BUILD_TYPE installd
+if [ -z "$PYTHON_DIST_PATH" ]; then export PYTHON_DIST_PATH=~/Documents/kivy/python-for-android/dist/default/; fi
+
+if [ -z "$BUILD_TYPE" ]; then export BUILD_TYPE=debug; fi
+
+
+cd ~/python-for-android/dist/default
+echo $PATH > testing
+python build.py --dir ~/domorereps-android/ --package org.domorereps.domorereps --name "Do More Reps" --version 1.0.0 --orientation portrait --permission INTERNET $BUILD_TYPE installr
