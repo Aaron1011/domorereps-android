d7ab0d490bf8b1f7eb59794204cdb54f338d031f
Aaron Hill <aa1ronham@gmail.com>
2013-05-27 20:13:03 -0400

    Made deploy.sh use environment variables

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
-export ANDROIDSDK=~/Downloads/adt-bundle-linux-x86_64-20130219/sdk/
-export ANDROIDAPI=8
-export ANDROIDNDK=~/Downloads/android-ndk-r8e
-export ANDROIDNDKVER=8
+$ANDROIDSDK || export ANDROIDSDK=~/Downloads/adt-bundle-linux-x86_64-20130219/sdk/
+$ANDROIDAPI || export ANDROIDAPI=8
+$ANDROIDNDK || export ANDROIDNDK=~/Downloads/android-ndk-r8e
+$ANDROIDNDKVER || export ANDROIDNDKVER=8
 
-export PYTHON_DIST_PATH=~/Documents/kivy/python-for-android/dist/default/
+$PYTHON_DIST_PATH || export PYTHON_DIST_PATH=~/Documents/kivy/python-for-android/dist/default/
+
+$BUILD_TYPE || export BUILD_TYPE=debug
 
 cd $PYTHON_DIST_PATH
-./build.py --dir ~/repos/domorereps-android/ --package org.domorereps.domorereps --name "Do More Reps" --version 1.0.0 --orientation portrait --permission INTERNET debug installd
+
+./build.py --dir ~/repos/domorereps-android/ --package org.domorereps.domorereps --name "Do More Reps" --version 1.0.0 --orientation portrait --permission INTERNET $BUILD_TYPE installd
