416fef882b14dc5587550e1ba149020179cace27
Aaron Hill <aa1ronham@gmail.com>
2013-05-27 20:13:16 -0400

    Added deploy_to_market.sh

---
+++ deploy_to_market.sh
@@ -0,0 +1,25 @@
+ssh ubuntu@50.16.231.161 /bin/sh <<\EOF
+. secrets.sh
+
+export ANDROIDSDK=~/android-tools/adt-bundle-linux-x86_64-20130514/sdk/
+export ANDROIDNDK=~/android-tools/android-ndk-r8e
+export PYTHON_DIST_PATH=~/python-for-android/dist/default/
+
+export PATH=$ANDROIDNDK:$ANDROIDSDK/tools:$PATH
+
+export BUILD_TYPE=release
+
+~/repos/domorereps-android/deploy.sh
+
+jarsigner -verbose -keypass $KEYSTORE_PASS -storepass $KEYSTORE_PASS -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.my-keystore.keystore
+ ~/python-for-android/dist/default/bin/DoMoreReps-1.0.0-unsigned.apk alias_name
+
+zipalign -f -v 4 ~/python-for-android/dist/default/bin/DoMoreReps-1.0.0-unsigned.apk DoMoreReps.apk
+
+curl http://testflightapp.com/api/builds.json
+  -F file=@DoMoreReps.apk
+  -F api_token=$TESTFLIGHT_API_TOKEN
+  -F team_token=$TESTFLIGHT_TEAM_TOKEN
+  -F notes=`git log -1 --pretty=%B`
+  -F notify=True 
+EOF
