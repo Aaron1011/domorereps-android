c7aedbd57dbad3e19797190304c062f25f17c473
Aaron Hill <aa1ronham@gmail.com>
2013-05-28 17:07:06 -0400

    Made weightless/weight + reps toggle buttons show current state when EditExercise screen is entered

--- screens.py
+++ screens.py
@@ -1,12 +1,16 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.properties import ObjectProperty
+from kivy.clock import Clock
 from models import *
 from session_decorator import *
 
 from sqlalchemy.orm.exc import MultipleResultsFound
+
+from types import MethodType
 
 class HomeScreen(Screen):
     pass
@@ -51,17 +55,50 @@ class ExercisesScreen(Screen):
 
         edit_screen.exercise = exercise
         edit_screen.name_input.text = exercise.name
+        if exercise.weightless and edit_screen.weightless_button.state == 'normal':
+            edit_screen.weightless_button.trigger_action()
+        elif not exercise.weightless and edit_screen.weights_button.state == 'normal':
+            edit_screen.weights_button.trigger_action()
+
         self.manager.current = 'editexercise'
          
+
+def trigger_action(self, duration=0.1):
+    '''Trigger whatever action(s) have been bound to the button by calling
+    both the on_press and on_release callbacks.
+
+    This simulates a quick button press without using any touch events.
+
+    Duration is the length of the press in seconds. Pass 0 if you want
+    the action to happen instantly.
+
+    .. versionadded:: 1.8.0
+    '''
+    self._do_press()
+    self.dispatch("on_press")
+    def trigger_release(dt):
+        self._do_release()
+        self.dispatch("on_release")
+    if not duration:
+        trigger_release(0)
+    else:
+        Clock.schedule_once(trigger_release, duration)
 
 class EditExerciseScreen(Screen):
     layout = ObjectProperty(None)
     exercise = ObjectProperty(None)
     name_input = ObjectProperty(None)
+    weightless_button = ObjectProperty(None)
+    weights_button = ObjectProperty(None)
 
     def __init__(self, session, **kwargs):
         super(EditExerciseScreen, self).__init__(**kwargs)
         self.session_class = session
+        self.weightless_button.trigger_action = MethodType(trigger_action,
+                self.weightless_button, ToggleButton)
+
+        self.weights_button.trigger_action = MethodType(trigger_action,
+                self.weights_button, ToggleButton)
 
     def save(self):
         with transactional_session(self.session_class) as session:
