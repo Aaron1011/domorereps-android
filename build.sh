7e62a1946b1aefe348915bd2c6f416a6998f48fa
Aaron Hill <aa1ronham@gmail.com>
2013-05-29 17:47:02 -0400

    Added versioning support to build scripts

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ PYTHON_DIST_PATH=~/python-for-android/dist/default
 PATH=$ANDROIDNDK:$ANDROIDSDK/tools:$PATH
 
 BUILD_TYPE=release
+VERSION=$1
 
 if [ -z "$ANDROIDSDK" ]; then export ANDROIDSDK=~/Downloads/adt-bundle-linux-x86_64-20130219/sdk/; fi
 if [ -z "$ANDROIDAPI" ]; then export ANDROIDAPI=8; fi
@@ -14,8 +15,8 @@ if [ -z "$ANDROIDNDKVER" ]; then export ANDROIDNDKVER=8; fi
 if [ -z "$PYTHON_DIST_PATH" ]; then export PYTHON_DIST_PATH=~/Documents/kivy/python-for-android/dist/default/; fi
 
 if [ -z "$BUILD_TYPE" ]; then export BUILD_TYPE=debug; fi
-
+if [ -z "$VERSION" ]; then export VERSION="1.0.0"; fi
 
 cd ~/python-for-android/dist/default
-echo $PATH > testing
-python build.py --dir ~/domorereps-android/ --package org.domorereps.domorereps --name "Do More Reps" --version 1.0.0 --orientation portrait --permission INTERNET $BUILD_TYPE release
+echo $VERSION
+python build.py --dir ~/domorereps-android/ --package org.domorereps.domorereps --name "Do More Reps" --version $VERSION --orientation portrait --permission INTERNET $BUILD_TYPE release
--- deploy_to_testflight.sh
+++ deploy_to_testflight.sh
@@ -13,12 +13,16 @@ PYTHON_DIST_PATH=~/python-for-android/dist/default;
 PATH=$ANDROIDNDK:$ANDROIDSDK/tools:$PATH;
 
 BUILD_TYPE=release;
+REPO_PATH="`realpath domorereps-android/.git`"
+VERSION="`git --git-dir=$REPO_PATH describe --abbrev=0 --tags`"
+VERSION="`echo $VERSION | cut -d "v" -f 2`"
 
-~/domorereps-android/build.sh
+echo $VERSION
+~/domorereps-android/build.sh VERSION
 
-jarsigner -verbose -keypass $KEYSTORE_PASS -storepass $KEYSTORE_PASS -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.my-keystore.keystore ~/python-for-android/dist/default/bin/DoMoreReps-1.0.0-release-unsigned.apk mykey;
+jarsigner -verbose -keypass $KEYSTORE_PASS -storepass $KEYSTORE_PASS -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.my-keystore.keystore ~/python-for-android/dist/default/bin/DoMoreReps-$VERSION-release-unsigned.apk mykey;
 
-zipalign -f -v 4 ~/python-for-android/dist/default/bin/DoMoreReps-1.0.0-release-unsigned.apk DoMoreReps.apk;
+zipalign -f -v 4 ~/python-for-android/dist/default/bin/DoMoreReps-$VERSION-release-unsigned.apk DoMoreReps.apk;
 
 curl "http://testflightapp.com/api/builds.json" \
   -F file=@DoMoreReps.apk \
